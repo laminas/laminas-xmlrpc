b081743381c64faba3afcfdbd7359766efd8ff3a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 12:06:32 -0600

    Rewriting as Laminas Project package

--- src/Client/Exception/ExceptionInterface.php
+++ src/Client/Exception/ExceptionInterface.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-xmlrpc for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-xmlrpc/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-xmlrpc/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\XmlRpc\Client\Exception;
+namespace Laminas\XmlRpc\Client\Exception;
 
-use Zend\XmlRpc\Exception\ExceptionInterface as Exception;
+use Laminas\XmlRpc\Exception\ExceptionInterface as Exception;
 
 /**
- * Base class for all Zend\XmlRpc\Client\* exceptions
+ * Base class for all Laminas\XmlRpc\Client\* exceptions
  */
 interface ExceptionInterface extends Exception
 {
