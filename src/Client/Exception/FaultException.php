2fa10b13a450b4766b0c851e86bf12e30cdc52f7
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 12:06:20 -0600

    Rewriting as Laminas Project package

--- src/Client/Exception/FaultException.php
+++ src/Client/Exception/FaultException.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-xmlrpc for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-xmlrpc/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-xmlrpc/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\XmlRpc\Client\Exception;
+namespace Laminas\XmlRpc\Client\Exception;
 
-use Zend\XmlRpc\Exception;
+use Laminas\XmlRpc\Exception;
 
 /**
- * Thrown by Zend\XmlRpc\Client when an XML-RPC fault response is returned.
+ * Thrown by Laminas\XmlRpc\Client when an XML-RPC fault response is returned.
  */
 class FaultException extends Exception\BadMethodCallException implements ExceptionInterface
 {
