7b5db2e70de60c2e38c09f1ad59c95e2d9348741
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:11:13 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/PhpInputMock.php
+++ test/PhpInputMock.php
@@ -1,10 +1,4 @@
 <?php // @codingStandardsIgnoreFile
-/**
- * @see       https://github.com/laminas/laminas-xmlrpc for the canonical source repository
- * @copyright https://github.com/laminas/laminas-xmlrpc/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-xmlrpc/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\XmlRpc;
 
 /**
