28bb0444aa7cba3dcdbf2d36abc70eeca12bc888
Andi Rückauer <arueckauer@gmail.com>
2021-10-11 13:09:00 +0200

    Apply laminas-coding-standard:2.2
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- test/autoload.php
+++ test/autoload.php
@@ -1,14 +1,9 @@
 <?php
 
 /**
- * @see       https://github.com/laminas/laminas-xmlrpc for the canonical source repository
- * @copyright https://github.com/laminas/laminas-xmlrpc/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-xmlrpc/blob/master/LICENSE.md New BSD License
- */
-
-/**
  * @todo This file may be removed once we drop support for PHP 5.
  */
+
 if (! class_exists(PHPUnit\Framework\ExpectationFailedException::class)) {
     class_alias(
         PHPUnit_Framework_ExpectationFailedException::class,
