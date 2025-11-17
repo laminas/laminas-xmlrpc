6385e09612ff81e7461bf0af32b1789377ee1e12
d.super <6484766+SuperDJ@users.noreply.github.com>
2025-11-17 17:10:21 +0100

    Fix CS
    Signed-off-by: d.super <6484766+SuperDJ@users.noreply.github.com>
    
     Author:    d.super <6484766+SuperDJ@users.noreply.github.com>
    
    Signed-off-by: d.super <6484766+SuperDJ@users.noreply.github.com>

--- src/Generator/GeneratorInterface.php
+++ src/Generator/GeneratorInterface.php
@@ -7,21 +7,27 @@ namespace Laminas\XmlRpc\Generator;
  */
 interface GeneratorInterface
 {
+    /**
+     * @return string
+     */
     public function getEncoding();
 
     /**
      * @param string $encoding
+     * @return static
      */
     public function setEncoding($encoding);
 
     /**
      * @param string $name
      * @param string $value
+     * @return AbstractGenerator
      */
     public function openElement($name, $value = null);
 
     /**
      * @param string $name
+     * @return AbstractGenerator
      */
     public function closeElement($name);
 
@@ -33,11 +39,14 @@ interface GeneratorInterface
     public function saveXml();
 
     /**
-     * @param  string $xml
+     * @param string $xml
      * @return string
      */
     public function stripDeclaration($xml);
 
+    /**
+     * @return string
+     */
     public function flush();
 
     public function __toString(): string;
